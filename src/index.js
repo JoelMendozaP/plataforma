b9bec26ee8b109d57099c895e849d300cb4e3c1c
JoelMendozaP <joelmendozapaucara@gmail.com>
2020-08-21 23:26:18 -0400

    resolviendo problemas

--- src/index.js
+++ src/index.js
@@ -9,13 +9,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  <script>
-  {document.cookie = 'same-site-cookie=foo; SameSite=Lax'}
-  {document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure'}
-  </script>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
